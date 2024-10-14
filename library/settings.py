7968fa11fa87b15853a1b78643040370683e134c
Elbek <juraquluvelbek13@gmail.com>
2024-10-14 10:23:33 +0500

    'gunicorn'

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wbysd#%9spo0on(luu35l0d@!2^a0bl(rultpc8f7o334qrh1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -151,6 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
